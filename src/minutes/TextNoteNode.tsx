492eac6ad2362d20d8e9d5ca1e393ee13605af45
Carter Butaud <cabutaud@microsoft.com>
2023-07-03 23:54:07 -0700

    added editing for text node content

--- src/minutes/TextNoteNode.tsx
+++ src/minutes/TextNoteNode.tsx
@@ -1,13 +1,53 @@
+import { useCallback, useState } from "react";
 import { TextNote } from "minute-model";
+import { NodeControls } from "./NodeControls";
 import { SpeakerReference } from "./SpeakerReference";
+import "./TextNoteNode.css";
 
 export const TextNoteNode: React.FC<{ note: TextNote }> = ({ note }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(note.text);
+
+  const handleTextChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setText(event.target.value);
+    },
+    []
+  );
+
+  const handleSave = useCallback(() => {
+    note.text = text;
+    setIsEditing(false);
+  }, [note, text]);
+
+  const handleCancel = useCallback(() => {
+    setText(note.text);
+    setIsEditing(false);
+  }, [note]);
+
   return (
-    <div>
-      <p>
-        {note.speaker && <SpeakerReference speaker={note.speaker} />}
-        {note.speaker && ":"} {note.text}
-      </p>
-    </div>
+    <NodeControls
+      isEditing={isEditing}
+      onEdit={() => setIsEditing(true)}
+      onSave={handleSave}
+      onCancel={handleCancel}
+    >
+      <div>
+        <p>
+          {note.speaker && <SpeakerReference speaker={note.speaker} />}
+          {note.speaker && ":"}{" "}
+          {isEditing ? (
+            <input
+              className="tnn-input"
+              type="text"
+              value={text}
+              onChange={handleTextChange}
+            />
+          ) : (
+            note.text
+          )}
+        </p>
+      </div>
+    </NodeControls>
   );
 };
