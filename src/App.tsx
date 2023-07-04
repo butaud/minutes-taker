eb69401fa23920e5099d7b052688b8b86c71d499
Carter Butaud <cabutaud@microsoft.com>
2023-07-04 00:09:14 -0700

    little cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Person, Session } from "minute-model";
 import { SessionEditor } from "./minutes/SessionEditor";
@@ -86,8 +84,6 @@ const fakeSession: Session = {
   ],
 };
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
