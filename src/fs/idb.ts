cbec08fb8b165e216093c4a554775d8536becc6e
Carter Butaud <cabutaud@microsoft.com>
2023-08-21 11:56:52 -0700

    handle no indexeddb case

--- src/fs/idb.ts
+++ src/fs/idb.ts
@@ -5,29 +5,37 @@ const OBJECT_STORE_NAME = "ObjectStore";
 
 const idbContext: { db?: IDBDatabase } = { db: undefined };
 export const initializeIdb = async (): Promise<void> => {
+  if (!("indexedDB" in window)) {
+    return Promise.resolve();
+  }
+
   if (idbContext.db) {
     return Promise.resolve();
-  } else {
-    return new Promise<void>((resolve, reject) => {
-      const open = indexedDB.open(INDEXEDDB_NAME, 2);
-      open.onupgradeneeded = () => {
-        open.result.deleteObjectStore(OBJECT_STORE_NAME);
-        open.result.createObjectStore(OBJECT_STORE_NAME);
-      };
-
-      open.onsuccess = () => {
-        idbContext.db = open.result;
-        resolve();
-      };
-
-      open.onerror = () => {
-        reject(open.error);
-      };
-    });
   }
+
+  return new Promise<void>((resolve, reject) => {
+    const open = indexedDB.open(INDEXEDDB_NAME, 2);
+    open.onupgradeneeded = () => {
+      open.result.deleteObjectStore(OBJECT_STORE_NAME);
+      open.result.createObjectStore(OBJECT_STORE_NAME);
+    };
+
+    open.onsuccess = () => {
+      idbContext.db = open.result;
+      resolve();
+    };
+
+    open.onerror = () => {
+      reject(open.error);
+    };
+  });
 };
 
 export const getIdb = async <T>(): Promise<T | undefined> => {
+  if (!("indexedDB" in window)) {
+    return Promise.resolve(undefined);
+  }
+
   await initializeIdb();
   return new Promise<T | undefined>((resolve, reject) => {
     const tx = idbContext.db!.transaction(OBJECT_STORE_NAME, "readonly");
@@ -43,6 +51,10 @@ export const getIdb = async <T>(): Promise<T | undefined> => {
 };
 
 export const setIdb = async <T>(value: T): Promise<void> => {
+  if (!("indexedDB" in window)) {
+    return Promise.resolve();
+  }
+
   await initializeIdb();
   return new Promise<void>((resolve, reject) => {
     const tx = idbContext.db!.transaction(OBJECT_STORE_NAME, "readwrite");
